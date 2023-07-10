da77230d24f020342ff0176986a67fac7d923274
cookie-s <kcz@kcz.sh>
2023-07-09 22:03:21 -0400

    mountain view

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -1,17 +1,20 @@
+const eMountainD3 = document.getElementById('mountain-d3');
 const draw = SVG()
     .attr('class', 'is-fullwidth is-fullheight')
     .addTo('#canvas');
+const SVGmountain = d3.create("svg")
+    .attr('class', 'is-fullwidth is-fullheight')
+    .attr("viewBox", [0, 0, 928, 600]);
 
 async function _submit({problem_id}) {
     draw.clear();
 
-    const problemId = problem_id;
-    if(!(problemId > 0)) {
+    if(!(problem_id > 0)) {
         throw 'invalid problem id';
     }
-    const inputData = await fetch(`../input/problem-${problemId}.json`).then(e => e.text());
+    const inputData = await fetch(`../input/problem-${problem_id}.json`).then(e => e.text());
     const input = JSON.parse(inputData);
-    const outputData = await fetch(`../output/problem-${problemId}.json`)
+    const outputData = await fetch(`../output/problem-${problem_id}.json`)
         .then(r => {
             if(!r.ok) throw 'err';
             return r.text();
@@ -68,6 +71,89 @@ async function _submit({problem_id}) {
     }
 }
 
+async function drawMountain({problem_id}) {
+    SVGmountain.selectAll("*").remove();
+    /***
+     * Base: Line Chart https://observablehq.com/@d3/line-chart/2?intent=fork
+     * Bar Chart https://observablehq.com/@d3/bar-chart/2?intent=fork
+     * https://d3js.org/getting-started#d3-in-vanilla-html
+     *
+     */
+
+    const dataRaw = await fetch(`../output/problem-${problem_id}.json.err`)
+        .then(r => {
+            if(!r.ok) throw 'err';
+            return r.text();
+        })
+        .catch(e => {
+            console.log(e);
+            return '';
+        });
+    const data = dataRaw
+        .split('\n')
+        .map(line => {
+            const match = line.match(/^trial (\d+): (\d+)$/);
+            if(!match) return false;
+            const [_, trial, score] = [...match].map(e => parseInt(e));
+            return {trial, score};
+        })
+        .filter(e => e);
+
+    // Declare the chart dimensions and margins.
+    const width = 928;
+    const height = 600;
+    const marginTop = 20;
+    const marginRight = 50;
+    const marginBottom = 30;
+    const marginLeft = 70;
+
+    const x = d3.scaleLinear([0, d3.max(data, d => d.trial)], [marginLeft, width - marginRight]);
+    const y = d3.scaleLinear([0, d3.max(data, d => d.score)], [height - marginBottom, marginTop]);
+
+    const line = d3.line()
+        .x(d => x(d.trial))
+        .y(d => y(d.score));
+
+    // x-axis
+    SVGmountain.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+    // y-axis
+    SVGmountain.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(d3.axisLeft(y).ticks(height / 40))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick line").clone()
+            .attr("x2", width - marginLeft - marginRight)
+            .attr("stroke-opacity", 0.1))
+        .call(g => g.append("text")
+            .attr("x", -marginLeft)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text("Score"));
+
+    SVGmountain.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 0.5)
+        .attr("d", line(data));
+
+    const dataInc = [];
+    for (const dat of data) {
+        const last = dataInc.slice(-1)[0] || {score: -1};
+        if(dat.score > last.score) dataInc.push(dat);
+    }
+    SVGmountain.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-width", 1)
+        .attr("d", line(dataInc));
+
+    eMountainD3.append(SVGmountain.node());
+}
+
 const app = new Vue({
     el: '#app',
     data: {
@@ -80,6 +166,9 @@ const app = new Vue({
     methods: {
         async submit() {
             await _submit({
+                problem_id: this.room_problemid,
+            });
+            await drawMountain({
                 problem_id: this.room_problemid,
             });
         },
