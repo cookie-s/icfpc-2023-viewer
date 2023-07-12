b93c567f9b3734db68f947521aee3f4481bd6e54
cookie-s <kcz@kcz.sh>
2023-07-12 08:26:31 -0400

    smallkirby

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -6,6 +6,15 @@ const SVGmountain = d3.create("svg")
     .attr('class', 'is-fullwidth is-fullheight')
     .attr("viewBox", [0, 0, 928, 600]);
 
+async function readErrData(problem_id) {
+    try {
+        return await fetch(`../output/problem-${problem_id}.json.err`).then(e => e.text());
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
 async function _submit({problem_id}) {
     draw.clear();
 
@@ -14,6 +23,7 @@ async function _submit({problem_id}) {
     }
     const inputData = await fetch(`../input/problem-${problem_id}.json`).then(e => e.text());
     const input = JSON.parse(inputData);
+
     const outputData = await fetch(`../output/problem-${problem_id}.json`)
         .then(r => {
             if(!r.ok) throw 'err';
@@ -21,6 +31,18 @@ async function _submit({problem_id}) {
         })
         .catch(_ => '{"placements":[], "volumes":[]}');
     const output = JSON.parse(outputData);
+
+    const dbgData = await readErrData(problem_id);
+    let _musicianScores = dbgData === null ? dbgData : dbgData.split("\n").map(line => {
+        const match = line.match(/^score (\d+): (\d+)$/);
+        if (!match) return false;
+        const [_, musician_id, score] = [...match].map(e => parseInt(e));
+        return {musician_id, score};
+    })
+    .filter(e => e);
+    const musicianScores = (_musicianScores === null || _musicianScores.length === 0) ? null : _musicianScores;
+
+    const g = draw.group();
 
     const room = {
         width: input['room_width'],
@@ -36,39 +58,90 @@ async function _submit({problem_id}) {
     };
 
     // room
-    draw.rect(room.width, room.height)
+    g.rect(room.width, room.height)
         .fill('#fff')
         .stroke('black');
 
     // stage
-    draw.rect(stage.width, stage.height)
+    g.rect(stage.width, stage.height)
         .move(stage.x, stage.y)
         .fill('#ddd');
 
+    // pillars
     const pillars = input['pillars'];
     for(const pillar of pillars) {
         const {center: [x, y], radius} = pillar;
-        draw.circle({cx: x, cy: y})
+        g.circle({cx: x, cy: y})
             .radius(radius).fill('#f00');
     }
 
+    // attendees
     const attendees = input['attendees'];
     for(const attendee of attendees) {
         const {x, y, _tastes} = attendee;
-        draw.circle({cx: x, cy: y})
+        g.circle({cx: x, cy: y})
             .radius(5).fill('#0f0');
     }
 
+    // musicians
+    //  - volume represented as transparecy
+    //  - score reperesented as color
     const placements = output['placements'];
     const volumes = output['volumes'];
+    const maxScore = musicianScores === null ? 0 : musicianScores.reduce((acc, cur) => Math.max(acc, cur.score), 0);
+    const minScore = musicianScores === null ? 0 : musicianScores.reduce((acc, cur) => Math.min(acc, cur.score), 0);
     for (let i = 0; i < placements.length; i++) {
         const {x, y} = placements[i];
         const volume = volumes[i];
 
-        draw.circle({cx: x, cy: y})
+        const {red,green,blue} = musicianScores === null ? {red: 0, green: 0, blue: 255} : (() => {
+            const score = musicianScores[i].score;
+            const scoreRatio = (score - minScore) / (maxScore - minScore);
+            const red = Math.floor(255 * scoreRatio);
+            const green = 0;
+            const blue = Math.floor(255 * (1 - scoreRatio));;
+            return {red, green, blue};
+        })();
+
+        const circle = g.circle({cx: x, cy: y})
             .radius(10)
-            .fill(`rgba(0, 0, 255, ${(1+volume)/20})`);
+            .fill(`rgba(${red}, ${green}, ${blue}, ${(1+volume)/20})`)
+            .on('mouseover', () => {
+                const tooltip = document.createElement('div');
+                const circlePosition = circle.node.getBoundingClientRect();
+                tooltip.classList.add('tooltip');
+                tooltip.style.position = 'fixed';
+                tooltip.style.top = `${circlePosition.top + 5}px`;
+                tooltip.style.left = `${circlePosition.left + 5}px`;
+                tooltip.innerHTML = `
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="content">
+                                <p>ID: ${i}</p>
+                                <p>Score: ${musicianScores === null ? 'N/A' : musicianScores[i].score}</p>
+                                <p>Volume: ${volume}</p>
+                                <p>Instrument: ${input['musicians'][i]}</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                document.body.appendChild(tooltip);
+            })
+            .on('mouseout', () => {
+                const tooltip = document.querySelector('.tooltip');
+                if(tooltip) {
+                    tooltip.remove();
+                }
+            });
     }
+
+    // Handle zoom
+    const handleZoom = (e) => {
+        d3.select("svg g").attr("transform", e.transform);
+    };
+    const zoom = d3.zoom()
+        .on("zoom", handleZoom);
+    d3.select("svg").call(zoom);
 }
 
 async function drawMountain({problem_id}) {
@@ -158,7 +231,7 @@ const app = new Vue({
     el: '#app',
     data: {
         tab: 'room',
-        room_problemid: 1,
+        room_problemid: 65, // TODO
     },
     mounted() {
         this.submit();
